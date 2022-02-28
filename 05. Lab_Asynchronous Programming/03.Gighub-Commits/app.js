80fa334379b335cdc2c17fc11b1b18e423b57ede
ivanadnin <ivandanin01@gmail.com>
2022-02-28 16:47:06 +0200

    async

--- 05. Lab_Asynchronous Programming/03.Gighub-Commits/app.js
+++ 05. Lab_Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,17 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let usernameElement = document.getElementById('username');
+	let repoElement = document.getElementById('repo');
+    let ulElement = document.getElementById('commits');
+
+    fetch(`https://api.github.com/repos/${usernameElement.value}/${repoElement.value}/commits`)
+    .then(response => 
+        response.json())
+    .then(result => {
+        result.forEach(({commit}) => {
+            let li = document.createElement('li');
+            li.innerHTML = `${commit.author.name}: ${commit.message}`;
+            ulElement.appendChild(li);
+        });
+    })
+	.catch(error => console.log(error));
 }
