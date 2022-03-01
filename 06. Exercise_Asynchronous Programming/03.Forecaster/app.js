ccd27765293b941db5b114e029a913db266d97c6
ivanadnin <ivandanin01@gmail.com>
2022-03-01 14:45:08 +0200

    async

--- 06. Exercise_Asynchronous Programming/03.Forecaster/app.js
+++ 06. Exercise_Asynchronous Programming/03.Forecaster/app.js
@@ -1,5 +1,26 @@
 function attachEvents() {
-    console.log("TODO...");
+    let locationElement = document.getElementById('location').value;
+    let submitBtn = document.getElementById('submit');
+
+    let forecast = document.getElementById('forecast');
+    let currentWeather = document.getElementById('current');
+    let upcomingWeather = document.getElementById('upcoming');
+
+    
+
+    submitBtn.addEventListener('click', () => {
+        
+        if (locationElement.value) {
+        let url = 'http://localhost:3030/jsonstore/forecaster/locations';
+        
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            return data.find(c => c.name.toLowerCase() == locationElement.toLowerCase()).code;
+        })
+    }
+    });
 }
 
 attachEvents();
