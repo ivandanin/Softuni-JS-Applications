bd6fa4cca11f006fea3df0906d2bd5f6c03a0073
ivanadnin <ivandanin01@gmail.com>
2022-03-08 13:51:05 +0200

    library

--- 08. Exercise_Data and Authentication/04.Book-library/app.js
+++ 08. Exercise_Data and Authentication/04.Book-library/app.js
@@ -1,1 +1,26 @@
-console.log('My requests...')
+let tableBodyElement = document.querySelector('#loadBooks tbody');
+
+function solve() {
+    document.querySelector('loadBooks').addEventListener('click', getAllBooks);
+
+}
+
+function getAllBooks(event) {
+    event.preventDefault();
+
+    fetch('http://localhost:3030/jsonstore/collections/books')
+    .then(response => response.json())
+    .then(data => {
+        Object.entries(data).forEach()
+    })
+}
+
+function createElement(type, text, appender) {
+    let newElement = document.createElement(type);
+    newElement.textContent = text;
+    appender.appendChild(newElement);
+
+    return newElement;
+}
+
+solve;
