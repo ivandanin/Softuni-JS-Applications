00f03351b20964b8fa01de37fe4f6bd337d3ee17
ivanadnin <ivandanin01@gmail.com>
2022-03-15 09:58:22 +0200

    table

--- 14. Exercise_Client  Side Rendering/05.Table-Search/solution.js
+++ 14. Exercise_Client  Side Rendering/05.Table-Search/solution.js
@@ -2,50 +2,49 @@ import { html, render } from "../node_modules/lit-html/lit-html.js";
 
 const url = 'http://localhost:3030/jsonstore/advanced/table';
 
+let template = (data) => html`
+${data.map(el => html`
+<tr id="${el._id}">
+<td>${el.firstName} ${el.lastName}</td>
+<td>${el.email}</td>
+<td>${el.course}</td>
+</tr>`)}`;
+
+let options = Object.values(await getOptions());
+let tBodyElement = document.querySelector('.container tbody');
+
 async function getOptions() {
-   let response = await fetch(url);
+   const response = await fetch(url);
    return await response.json();
 }
 
+async function update() {
+   render(template(options), tBodyElement);
+}
+
+
 function solve() {
-
+   
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
-   let template = (data) => html`
-   ${data.map(el => html`
-   <tr id="${el._id}">
-      <td>${el.firstName} ${el.lastName}</td>
-      <td>${el.email}</td>
-      <td>${el.course}</td>
-   </tr>`)}`;
-
-   let options = Object.values(getOptions());
-   let tBodyElement = document.querySelector('.container tbody');
-
-   update(options);
-
-   function update(options) {
-      render(template(options), tBodyElement);
-   }
-
+   
    async function onClick(event) {
       event.preventDefault();
 
       let trElements = document.querySelectorAll('tbody tr');
       let searchField = document.querySelector('#searchField');
 
-      trElements.forEach(r => r.removeAttribute('class', 'select'));
-
+      for (const row of trElements) {
+         row.removeAttribute('class', 'select');
+      }
       if (searchField.value) {
-         trElements.forEach(r => {
-            if (r.innerHTML.includes(searchField.value)) {
-               r.setAttribute('class', 'select');
-            }
-            update(options);
-            searchField.value = '';
-         });
+         for (const row of trElements) {
+            if (row.innerHTML.includes(searchField.value)) {
+            row.setAttribute('class', 'select');
+         }
       }
    }
+   searchField.value = '';
 }
-
+}
+update();
 solve();
