29a4aa471f6e4e6f65bf58bc93bd41b054786be7
ivanadnin <ivandanin01@gmail.com>
2022-03-18 17:10:36 +0200

    exam prep

---
+++ JS Applications Exam Prep/JS Applications Exam - Meme Lounge/Meme_Lounge/src/app.js
@@ -0,0 +1,47 @@
+import { page, render } from './lib.js';
+import { homePage } from './views/home.js';
+import { catalogPage } from './views/catalog.js';
+import {loginPage} from './views/login.js';
+import { registerPage } from './views/register.js';
+
+// used for debug
+import * as api from './api/data.js';
+import { logout } from './api/api.js';
+import { getUserData } from './util.js';
+window.api = api;
+
+const root = document.querySelector('main');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/memes', catalogPage);
+page('/login', loginPage);
+page('/register', registerPage);
+
+updateUserNav();
+page.start();
+
+function decorateContext(context, next) {
+    context.render = (content) => render(content, root);
+    context.updateUserNav = updateUserNav;
+    next();
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+
+    if (userData) {
+        document.querySelector('.user').style.display = 'block';
+        document.querySelector('.guest').style.display = 'none';
+        document.querySelector('.user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = 'block';
+    }
+}
